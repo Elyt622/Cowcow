d026df43a93477274e865c088b2b2ac3f8775454
Yohan <eras.projet@gmail.com>
2023-07-16 16:24:17 +0200

    Added new function into mvxApi

--- app/src/main/java/com/example/hellocowcow/datas/network/api/MvxApi.kt
+++ app/src/main/java/com/example/hellocowcow/datas/network/api/MvxApi.kt
@@ -1,10 +1,11 @@
 package com.example.hellocowcow.datas.network.api
 
-import com.example.hellocowcow.datas.response.TokenResponse
+import com.example.hellocowcow.datas.response.mvxApi.NftResponse
+import com.example.hellocowcow.datas.response.mvxApi.TokenResponse
+import com.example.hellocowcow.datas.response.mvxApi.TransactionsResponse
 import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Path
-import javax.inject.Singleton
 
 interface MvxApi {
 
@@ -13,4 +14,20 @@ interface MvxApi {
         @Path("identifier") identifier: String
     ) : Single<TokenResponse>
 
+    @GET("/accounts/{address}/transactions?receiver={stakingAddress}&status=success&function=stake")
+    fun getAllCowsStaked(
+        @Path("address") address: String,
+        @Path("stakingAddress") stakingAddress: String
+    ) : Single<List<TransactionsResponse>>
+
+    @GET("/accounts/{address}/transactions?receiver={stakingAddress}&status=success&function=unstake")
+    fun getAllCowsUnStaked(
+        @Path("address") address: String,
+        @Path("stakingAddress") stakingAddress: String
+    ) : Single<List<TransactionsResponse>>
+
+    @GET ("/accounts/{address}/nfts?collections=COW-cd463d")
+    fun getAllCowsInWallet(
+        @Path("address") address: String
+    ) : Single<List<NftResponse>>
 }
