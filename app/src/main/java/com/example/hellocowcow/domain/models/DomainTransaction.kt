e739182d96a688de215588ce205dbe5210b5d213
Yohan <eras.projet@gmail.com>
2023-07-16 16:19:51 +0200

    Add a domain for mapping responses

---
+++ app/src/main/java/com/example/hellocowcow/domain/models/DomainTransaction.kt
@@ -0,0 +1,19 @@
+package com.example.hellocowcow.domain.models
+
+import com.example.hellocowcow.datas.response.mvxApi.ActionResponse
+
+data class DomainTransaction(
+    var txHash: String? = null,
+    var receiver: String? = null,
+    var sender: String? = null,
+    var value: String? = null,
+    var fee: String? = null,
+    var timestamp: Int? = null,
+    var data: String? = null,
+    var function: String? = null,
+    var action: ActionResponse? = ActionResponse(),
+    var type: String? = null,
+    var originalTxHash: String? = null,
+    var price: Double? = null,
+    var operations: ArrayList<DomainOperation>? = null
+)
