6d1fe9331ba56767721b2e181a8e7984f2b97f05
Yohan <eras.projet@gmail.com>
2023-07-19 01:52:04 +0200

    Add new function getTotalRewardsToCollect

--- app/src/main/java/com/example/hellocowcow/datas/repositories/TokenRepositoryImpl.kt
+++ app/src/main/java/com/example/hellocowcow/datas/repositories/TokenRepositoryImpl.kt
@@ -1,6 +1,8 @@
 package com.example.hellocowcow.datas.repositories
 
 import com.example.hellocowcow.datas.network.api.MvxApi
+import com.example.hellocowcow.datas.response.mvxApi.RewardRequest
+import com.example.hellocowcow.domain.models.DomainReward
 import com.example.hellocowcow.domain.models.DomainToken
 import com.example.hellocowcow.domain.repositories.TokenRepository
 import io.reactivex.rxjava3.core.Single
@@ -14,5 +16,12 @@ class TokenRepositoryImpl @Inject constructor(
         id: String
     ): Single<DomainToken> =
         mvxApi.getToken(id).map { it.toDomain() }
+
+    override
+    fun getTotalRewardsToCollect(
+        rewardRequest: RewardRequest
+    ) : Single<DomainReward> =
+        mvxApi.getTotalRewardsToCollect(rewardRequest)
+            .map { it.toDomain() }
 
 }
--- app/src/main/java/com/example/hellocowcow/domain/repositories/TokenRepository.kt
+++ app/src/main/java/com/example/hellocowcow/domain/repositories/TokenRepository.kt
@@ -1,10 +1,16 @@
 package com.example.hellocowcow.domain.repositories
 
+import com.example.hellocowcow.datas.response.mvxApi.RewardRequest
+import com.example.hellocowcow.domain.models.DomainReward
 import com.example.hellocowcow.domain.models.DomainToken
 import io.reactivex.rxjava3.core.Single
 
 interface TokenRepository {
 
     fun getToken(id: String) : Single<DomainToken>
+
+    fun getTotalRewardsToCollect(
+        rewardRequest: RewardRequest
+    ) : Single<DomainReward>
 
 }
