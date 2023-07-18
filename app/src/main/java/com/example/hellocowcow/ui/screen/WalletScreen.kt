3755156b79ef30ebb76c8729deb009ae65533b32
Yohan <eras.projet@gmail.com>
2023-07-19 01:46:01 +0200

    Add WalletScreen & WalletViewModel for tab in ProfileScreen

---
+++ app/src/main/java/com/example/hellocowcow/ui/screen/WalletScreen.kt
@@ -0,0 +1,123 @@
+package com.example.hellocowcow.ui.screen
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.hellocowcow.ui.theme.Typography2
+import com.example.hellocowcow.ui.viewmodels.screen.WalletViewModel
+
+@OptIn(ExperimentalGlideComposeApi::class)
+@Composable
+fun WalletScreen(
+    viewModel: WalletViewModel
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    val cardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = Color.Black
+    )
+
+    viewModel.getAllCowsInWallet("erd10t86rsk3rzaap7gqkcgp930d24rw8wusx8e6z4ncfgqxqp3s29ss4fvkz6")
+
+    when (uiState) {
+        is WalletViewModel.UiState.Success -> {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 50.dp),
+                columns = GridCells.Adaptive(150.dp),
+                content = {
+                    items((uiState as WalletViewModel.UiState.Success).data) { nft ->
+                        ElevatedCard(
+                            colors = cardColors,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            GlideImage(
+                                model = nft.url,
+                                contentDescription = nft.collection,
+                                modifier = Modifier
+                                    .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                            )
+
+                            /*GlideImage(
+                                model = "https://xoxno.com/api/getCow?identifier=" + nft.identifier,
+                                contentDescription = nft.collection,
+                                modifier = Modifier
+                                    .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                            )*/
+
+                            Column(
+                                Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .padding(8.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    color = MaterialTheme.colorScheme.background,
+                                    text = nft.name.toString(),
+                                    style = Typography2.bodyLarge
+                                )
+                                Text(
+                                    color = MaterialTheme.colorScheme.background,
+                                    text = "Rank : " + nft.rank.toString(),
+                                    style = Typography2.labelMedium
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        }
+        is WalletViewModel.UiState.Loading -> Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(60.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+        is WalletViewModel.UiState.Error -> {
+            Toast.makeText(
+                LocalContext.current,
+                (uiState as WalletViewModel.UiState.Error).error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        is WalletViewModel.UiState.NoData -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No NFT in the wallet !",
+                    style = Typography2.bodyLarge
+                )
+            }
+        }
+    }
+
+
+}
+
---
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/WalletViewModel.kt
@@ -0,0 +1,42 @@
+package com.example.hellocowcow.ui.viewmodels.screen
+
+import androidx.lifecycle.ViewModel
+import com.example.hellocowcow.datas.response.mvxApi.NftResponse
+import com.example.hellocowcow.domain.repositories.NftRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class WalletViewModel @Inject constructor(
+    private val nftRepository: NftRepository
+) : ViewModel() {
+
+    sealed class UiState {
+        object NoData : UiState()
+        object Loading : UiState()
+        data class Success(val data: List<NftResponse>) : UiState()
+        data class Error(val error: String) : UiState()
+    }
+
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState
+
+    fun getAllCowsInWallet(
+        address: String
+    ) = nftRepository.getAllCowsInWallet(address)
+        .subscribeBy (
+            onSuccess = { nfts ->
+                if (nfts.isNotEmpty())
+                    _uiState.value = UiState.Success(nfts)
+                else
+                    _uiState.value = UiState.NoData
+            },
+            onError = { error ->
+                _uiState.value = UiState.Error(error.message.toString())
+            }
+        )
+
+}
