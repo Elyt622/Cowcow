552e6f10c987cc40bc00714e345be515d7f5b873
Yohan <eras.projet@gmail.com>
2023-07-19 01:46:32 +0200

    Add StakeScreen & StakeViewModel for tab in ProfileScreen

---
+++ app/src/main/java/com/example/hellocowcow/ui/screen/StakeScreen.kt
@@ -0,0 +1,124 @@
+package com.example.hellocowcow.ui.screen
+
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.hellocowcow.ui.theme.Typography2
+import com.example.hellocowcow.ui.viewmodels.screen.StakeViewModel
+
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalFoundationApi::class)
+@Composable
+fun StakeScreen(
+    viewModel: StakeViewModel
+) {
+
+    val uiState by viewModel.uiState.collectAsState()
+
+    val cardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = Color.Black
+    )
+    val pagerState = rememberPagerState()
+
+    when (uiState) {
+        is StakeViewModel.UiState.Success -> {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(start = 8.dp, end = 8.dp, bottom = 40.dp),
+                columns = GridCells.Adaptive(150.dp),
+                content = {
+                    items((uiState as StakeViewModel.UiState.Success).data) { nft ->
+                        ElevatedCard(
+                            colors = cardColors,
+                            modifier = Modifier.padding(8.dp)
+                        ) {
+                            HorizontalPager(
+                                state = pagerState,
+                                pageCount = 1,
+                           ) { index ->
+                                GlideImage(
+                                    model = nft.url,
+                                    contentDescription = nft.collection,
+                                    modifier = Modifier
+                                        .padding(start = 8.dp, end = 8.dp, top = 8.dp)
+                                )
+                            }
+                            Column(
+                                Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .padding(8.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    color = MaterialTheme.colorScheme.background,
+                                    text = nft.name.toString(),
+                                    style = Typography2.bodyLarge
+                                )
+                                Text(
+                                    color = MaterialTheme.colorScheme.background,
+                                    text = "Rank : " + nft.rank.toString(),
+                                    style = Typography2.labelMedium
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        }
+        is StakeViewModel.UiState.Loading -> Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(60.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+        is StakeViewModel.UiState.Error -> {
+            Toast.makeText(
+                LocalContext.current,
+                (uiState as StakeViewModel.UiState.Error).error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        is StakeViewModel.UiState.NoData -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No NFT in staking !",
+                    style = Typography2.bodyLarge
+                )
+            }
+        }
+    }
+}
+
+
+
+
+
---
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/StakeViewModel.kt
@@ -0,0 +1,127 @@
+package com.example.hellocowcow.ui.viewmodels.screen
+
+import androidx.lifecycle.ViewModel
+import com.example.hellocowcow.datas.response.mvxApi.NftResponse
+import com.example.hellocowcow.datas.response.mvxApi.RewardRequest
+import com.example.hellocowcow.domain.repositories.NftRepository
+import com.walletconnect.util.bytesToHex
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.Single
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.schedulers.Schedulers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import java.net.HttpURLConnection
+import java.net.URL
+import javax.inject.Inject
+import kotlin.io.encoding.Base64
+import kotlin.io.encoding.ExperimentalEncodingApi
+
+@HiltViewModel
+class StakeViewModel @Inject constructor(
+    private val nftRepository: NftRepository
+) : ViewModel() {
+
+    init {
+        getAllDataUsers()
+    }
+
+    sealed class UiState {
+        object NoData : UiState()
+        object Loading : UiState()
+        data class Success(val data: List<NftResponse>) : UiState()
+        data class Error(val error: String) : UiState()
+    }
+
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState
+
+    @OptIn(ExperimentalEncodingApi::class)
+    fun getAllDataUsers() =
+        nftRepository.getAllDataUsers(
+            RewardRequest(
+                "erd1qqqqqqqqqqqqqpgqqgzzsl0re9e3u0t3mhv3jwg6zu63zssd7yqs3uu9jk",
+                "getAllDataForUser",
+                "0",
+                arrayListOf(),
+                "erd1xwnpvwxdwt4ptnmwhyygfm5gzafu2fwxchax9evf6myk3cr00xpsq6np2y"
+            )
+        )
+            .subscribeOn(Schedulers.io())
+            .map { data ->
+                val segmentedHexList = mutableListOf<String>()
+                val decoded: String = Base64.decode(data.returnData[0]).bytesToHex()
+                for (i in decoded.indices step 4) {
+                    val endIndex = kotlin.math.min(i + 4, decoded.length)
+                    val segment = decoded.substring(i, endIndex)
+                    if (!segment.contains("0000"))
+                        segmentedHexList.add(segment)
+                }
+                val destinationArray = segmentedHexList
+                    .subList(
+                        1,
+                        segmentedHexList[0].toInt(16) + 1)
+                    .toList()
+                destinationArray
+            }.map { list ->
+
+                val identifiersStr = StringBuilder()
+                identifiersStr.append("[")
+
+                for (element in list) {
+                    if (element != list.last())
+                        identifiersStr.append("COW-cd463d-$element,")
+                    else
+                        identifiersStr.append("COW-cd463d-$element]")
+                }
+                val single1 = nftRepository.getCowsWithCollection(identifiersStr.toString(), 100, 0).toObservable()
+                val single2 = nftRepository.getCowsWithCollection(identifiersStr.toString(), 100, 25).toObservable()
+                val single3 = nftRepository.getCowsWithCollection(identifiersStr.toString(), 100, 50).toObservable()
+                Observable.merge(single1, single2, single3)
+                    .subscribeOn(Schedulers.io())
+            }.subscribeOn(Schedulers.io())
+            .subscribeBy {
+                it.subscribeBy (
+                    onNext = { nfts ->
+                        if (nfts.isEmpty())
+                            _uiState.value = UiState.NoData
+                        else
+                            _uiState.value = UiState.Success(nfts)
+                    },
+                    onError = { error ->
+                        _uiState.value = UiState.Error(error.message.toString())
+                    }
+                ).isDisposed
+            }
+
+     private fun checkImageExists(
+         imageUrl: String
+     ): Single<Boolean> =
+         Single.just(try {
+            val url = URL(imageUrl)
+            val connection = url.openConnection() as HttpURLConnection
+            connection.requestMethod = "HEAD"
+            connection.connect()
+            val responseCode = connection.responseCode
+            responseCode == HttpURLConnection.HTTP_OK
+        } catch (e: Exception) {
+            false
+        })
+
+    fun loadImages(
+        nft: NftResponse
+    ) : Single<Array<String>> =
+        checkImageExists("https://xoxno.com/api/getCow?identifier=${nft.identifier}")
+            .map { imageExist ->
+                if (imageExist)
+                    arrayOf(
+                        nft.url.toString(),
+                        "https://xoxno.com/api/getCow?identifier=${nft.identifier}"
+                    )
+                else
+                    arrayOf(nft.url.toString())
+            }
+
+
+}
