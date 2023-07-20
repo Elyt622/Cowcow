0e636d1da2c466356c8eee1541b241a94e4cad78
Yohan <eras.projet@gmail.com>
2023-07-21 01:09:23 +0200

    Add Inject constructor

--- app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/NftViewModel.kt
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/NftViewModel.kt
@@ -2,8 +2,11 @@ package com.example.hellocowcow.ui.viewmodels.screen
 
 import androidx.lifecycle.ViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
 @HiltViewModel
-class NftViewModel : ViewModel() {
+class NftViewModel @Inject constructor(
+
+) : ViewModel() {
 
 }
