40f7f53e6ffc23f7ac5cb47e1fce3d24a5400b84
Yohan <eras.projet@gmail.com>
2023-07-21 01:01:21 +0200

    Added function getNft with xoxno api

--- app/src/main/java/com/example/hellocowcow/datas/network/api/XoxnoApi.kt
+++ app/src/main/java/com/example/hellocowcow/datas/network/api/XoxnoApi.kt
@@ -1,7 +1,15 @@
 package com.example.hellocowcow.datas.network.api
 
+import com.example.hellocowcow.datas.response.xoxnoApi.NftResponse
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface XoxnoApi {
 
-
+    @GET("/nfts/{identifier}")
+    fun getNft(
+        @Path("identifier") identifier: String
+    ) : Single<NftResponse>
 
 }
