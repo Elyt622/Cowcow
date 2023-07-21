1cfdc0b60e5a21d36a99005e53edfd4259d32646
Yohan <eras.projet@gmail.com>
2023-07-21 15:57:05 +0200

    Add function to get Nft with xoxno and add state to update the ui

--- app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/NftViewModel.kt
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/NftViewModel.kt
@@ -1,12 +1,39 @@
 package com.example.hellocowcow.ui.viewmodels.screen
 
 import androidx.lifecycle.ViewModel
+import com.example.hellocowcow.datas.response.xoxnoApi.NftResponse
+import com.example.hellocowcow.domain.repositories.NftRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class NftViewModel @Inject constructor(
-
+    private val nftRepository: NftRepository
 ) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState
+
+    sealed class UiState {
+        object Loading : UiState()
+        class Success(val nft: NftResponse) : UiState()
+        class Error(val error: String) : UiState()
+    }
+
+    fun getNft(
+        identifier: String
+    ) = nftRepository.getNftXoxno(identifier)
+            .subscribeBy (
+                onSuccess = { nft ->
+                    _uiState.value = UiState.Success(nft)
+                },
+                onError = { error ->
+                    _uiState.value = UiState.Error(error.message.toString())
+                }
+            )
+
 
 }
