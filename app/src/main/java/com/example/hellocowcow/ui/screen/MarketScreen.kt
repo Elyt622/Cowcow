e7c4b30f009e30537db0a74f0fee72a3a2105a1b
Yohan <eras.projet@gmail.com>
2023-07-23 21:37:28 +0200

    Update MarketScreen & viewmodel

--- app/src/main/java/com/example/hellocowcow/ui/screen/MarketScreen.kt
+++ app/src/main/java/com/example/hellocowcow/ui/screen/MarketScreen.kt
@@ -1,27 +1,130 @@
 package com.example.hellocowcow.ui.screen
 
-import androidx.compose.foundation.layout.Arrangement
+import android.content.Intent
+import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.hellocowcow.app.module.nft.NftActivity
+import com.example.hellocowcow.ui.theme.Typography2
 import com.example.hellocowcow.ui.viewmodels.screen.MarketViewModel
 
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MarketScreen(
     viewModel: MarketViewModel
 ) {
+    val uiState by viewModel.uiState.collectAsState()
+    val context = LocalContext.current
+    viewModel.getCowsListing()
+    val cardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = Color.Black
+    )
 
-    Column(
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(text = "Soon", style = MaterialTheme.typography.bodyLarge)
+    when (uiState) {
+        is MarketViewModel.UiState.Success -> {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(
+                    start = 8.dp,
+                    end = 8.dp,
+                    bottom = 40.dp
+                ),
+                columns = GridCells.Adaptive(150.dp),
+                content = {
+                    items((uiState as MarketViewModel.UiState.Success).data) { nft ->
+                        ElevatedCard(
+                            colors = cardColors,
+                            modifier = Modifier.padding(8.dp),
+                            elevation = CardDefaults.cardElevation(8.dp),
+                            onClick = {
+                                val intent = Intent(
+                                    context,
+                                    NftActivity::class.java
+                                )
+                                intent.putExtra("IDENTIFIER", nft.identifier)
+                                context.startActivity(intent)
+                            }
+                        ) {
+                            GlideImage(
+                                model = nft.url,
+                                contentDescription = nft.collection,
+                                modifier = Modifier
+                                    .padding(
+                                        start = 8.dp,
+                                        end = 8.dp,
+                                        top = 8.dp
+                                    )
+                            )
+
+                            Column(
+                                Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .padding(8.dp),
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                                Text(
+                                    color = MaterialTheme.colorScheme.background,
+                                    text = nft.name.toString(),
+                                    style = Typography2.bodyLarge
+                                )
+                            }
+                        }
+                    }
+                }
+            )
+        }
+
+        is MarketViewModel.UiState.Loading -> Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.width(60.dp),
+                color = MaterialTheme.colorScheme.primary
+            )
+        }
+
+        is MarketViewModel.UiState.Error -> {
+            Toast.makeText(
+                LocalContext.current,
+                (uiState as MarketViewModel.UiState.Error).error,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+
+        is MarketViewModel.UiState.NoData -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "No NFT on the market place !",
+                    style = Typography2.bodyLarge
+                )
+            }
+        }
     }
-    
 }
 
--- app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/MarketViewModel.kt
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/MarketViewModel.kt
@@ -1,14 +1,57 @@
 package com.example.hellocowcow.ui.viewmodels.screen
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import com.example.hellocowcow.data.response.xoxnoApi.NftResponse
+import com.example.hellocowcow.domain.repositories.NftRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class MarketViewModel @Inject constructor(
-
+    private val nftRepository: NftRepository
 ) : ViewModel() {
 
+    private val _address = mutableStateOf("")
+    val address: State<String> get() = _address
 
+    fun setAddress(value: String) {
+        _address.value = value
+    }
+
+    sealed class UiState {
+        object NoData : UiState()
+        object Loading : UiState()
+        data class Success(val data: List<NftResponse>) : UiState()
+        data class Error(val error: String) : UiState()
+    }
+
+    private val _uiState: MutableStateFlow<UiState> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState> = _uiState
+
+    fun getCowsListing() =
+        nftRepository.getCowsListing(address.value)
+        .map {
+            if (it.groupedByCollection.isEmpty())
+                _uiState.value = UiState.NoData
+            it.groupedByCollection
+        }
+        .flatMapIterable { it }
+        .filter { collection -> collection.ticker == "COW-cd463d" }
+        .subscribeBy (
+                onNext = {
+                    Timber.tag("DEBUGGG").d(it.ticker)
+                    if (it.nfts.isNotEmpty())
+                        _uiState.value = UiState.Success(it.nfts.toList())
+                },
+                onError = {
+                    _uiState.value = UiState.Error(it.message.toString())
+                }
+            ).isDisposed
 
 }
