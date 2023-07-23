fb8ef1b618cd5dbafeeac19bca9ac83af3df7d64
Yohan <eras.projet@gmail.com>
2023-07-23 20:17:56 +0200

    Optimize import

--- app/src/main/java/com/example/hellocowcow/ui/screen/ProfileScreen.kt
+++ app/src/main/java/com/example/hellocowcow/ui/screen/ProfileScreen.kt
@@ -18,10 +18,8 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
 import com.example.hellocowcow.ui.viewmodels.screen.ProfileViewModel
 
-@OptIn(ExperimentalGlideComposeApi::class)
 @Composable
 fun ProfileScreen(
     viewModel: ProfileViewModel,
