7f4d4e5f489414f273f024f349eb1ee36ed51d36
Yohan <eras.projet@gmail.com>
2023-07-28 17:03:35 +0200

    Update NftResponse to DomainNft

--- app/src/main/java/com/example/hellocowcow/domain/models/DomainNft.kt
+++ app/src/main/java/com/example/hellocowcow/domain/models/DomainNft.kt
@@ -1,9 +1,7 @@
 package com.example.hellocowcow.domain.models
 
 import com.example.hellocowcow.data.response.xoxnoApi.MetadataResponse
-import com.example.hellocowcow.data.response.xoxnoApi.NftValueResponse
 import com.example.hellocowcow.data.response.xoxnoApi.OffersInfoResponse
-import com.example.hellocowcow.data.response.xoxnoApi.OriginalMediaResponse
 import com.example.hellocowcow.data.response.xoxnoApi.SaleInfoNftResponse
 
 data class DomainNft (
@@ -21,25 +19,20 @@ data class DomainNft (
     var tags                 : ArrayList<String> = arrayListOf(),
     var avifUrl              : String?           = null,
     var webpUrl              : String?           = null,
-    var wasProcessed         : Boolean?          = null,
-    var id                   : String?           = null,
-    var rid                  : String?           = null,
     var onSale               : Boolean?           = null,
     var lastPrice            : String?           = null,
     var lastToken            : String?           = null,
     var hasOffers            : String?           = null,
     var retries              : Int?              = null,
-    var hasSecondNFT         : Boolean?          = null,
+    var hasSecondNFT         : Boolean?          = false,
     var metadata             : MetadataResponse? = MetadataResponse(),
-    var originalMedia        : OriginalMediaResponse? = OriginalMediaResponse(),
     var saleInfoNft          : SaleInfoNftResponse? = SaleInfoNftResponse(),
     var offersInfo           : ArrayList<OffersInfoResponse> = arrayListOf(),
     var owner                : String?           = null,
-    var ownerUsername        : String?           = null,
-    var isVerified           : Boolean?          = null,
-    var collectionName       : String?           = null,
-    var isVisible            : Boolean?          = null,
-    var nftValue             : NftValueResponse? = NftValueResponse(),
-    var ticker               : String?           = null,
+    var floorValue           : Double?           = null,
+    var avgValue             : Double?           = null,
+    var maxValue             : Double?           = null,
+    var collectionFp         : Double?           = null,
+    var ticker               : String?           = null
 
 )
