6b142e2618d006a94229c279993a171cb258e287
Yohan <eras.projet@gmail.com>
2023-07-30 19:51:46 +0200

    Use LaunchedEffect to use the viewmodel function once

--- app/src/main/java/com/example/hellocowcow/ui/screen/profile/MarketScreen.kt
+++ app/src/main/java/com/example/hellocowcow/ui/screen/profile/MarketScreen.kt
@@ -20,8 +20,11 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -40,11 +43,21 @@ fun MarketScreen(
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
-    viewModel.getCowsListing()
+
     val cardColors = CardDefaults.cardColors(
         containerColor = MaterialTheme.colorScheme.primary,
         contentColor = Color.Black
     )
+
+    val functionExecuted = remember { mutableStateOf(false) }
+
+    // LaunchedEffect runs when the Composable starts up
+    LaunchedEffect(Unit) {
+        if (!functionExecuted.value) {
+            viewModel.getCowsListing()
+            functionExecuted.value = true
+        }
+    }
 
     when (uiState) {
         is MarketViewModel.UiState.Success -> {
