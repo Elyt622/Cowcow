5e8fa6e1b051093addc56098ad2c6ef497637f96
Yohan <eras.projet@gmail.com>
2023-08-01 23:46:09 +0200

    Remove useless injection for cc tools repository

--- app/src/main/java/com/example/hellocowcow/app/di/repository/RepositoryModule.kt
+++ app/src/main/java/com/example/hellocowcow/app/di/repository/RepositoryModule.kt
@@ -1,12 +1,10 @@
 package com.example.hellocowcow.app.di.repository
 
 import com.example.hellocowcow.data.repositories.AccountRepositoryImpl
-import com.example.hellocowcow.data.repositories.CCToolsRepositoryImpl
 import com.example.hellocowcow.data.repositories.NftRepositoryImpl
 import com.example.hellocowcow.data.repositories.TokenRepositoryImpl
 import com.example.hellocowcow.data.repositories.TransactionRepositoryImpl
 import com.example.hellocowcow.domain.repositories.AccountRepository
-import com.example.hellocowcow.domain.repositories.CCToolsRepository
 import com.example.hellocowcow.domain.repositories.NftRepository
 import com.example.hellocowcow.domain.repositories.TokenRepository
 import com.example.hellocowcow.domain.repositories.TransactionRepository
@@ -29,11 +27,6 @@ abstract class RepositoryModule {
     abstract fun bindTransactionRepository(
         transactionRepositoryImpl: TransactionRepositoryImpl
     ) : TransactionRepository
-
-    @Binds
-    abstract fun bindCCToolsRepository(
-        ccToolsRepositoryImpl: CCToolsRepositoryImpl
-    ) : CCToolsRepository
 
     @Binds
     abstract fun bindNftRepository(
