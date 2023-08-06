1686b9d46cf0cca47a59710bee5c7fc8a41ec637
Yohan <eras.projet@gmail.com>
2023-08-06 20:24:13 +0200

    Replace fields by not null element for data class domain account

--- app/src/main/java/com/example/hellocowcow/domain/models/DomainAccount.kt
+++ app/src/main/java/com/example/hellocowcow/domain/models/DomainAccount.kt
@@ -1,14 +1,14 @@
 package com.example.hellocowcow.domain.models
 
 data class DomainAccount (
-    var address         : String = "",
-    var balance         : String? = null,
-    var nonce           : Int?    = null,
-    var timestamp       : Int?    = null,
-    var shard           : Int?    = null,
-    var rootHash        : String? = null,
-    var txCount         : Int?    = null,
-    var scrCount        : Int?    = null,
-    var username        : String = "",
+    var address : String = "",
+    var balance : String? = null,
+    var nonce : Int = 0,
+    var timestamp : Int? = null,
+    var shard : Int? = null,
+    var rootHash : String? = null,
+    var txCount : Int? = null,
+    var scrCount : Int? = null,
+    var username : String = "",
     var developerReward : String? = null
 )
