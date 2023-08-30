62aa712d4fe5331c9c512ea29329505828352516
Yohan <eras.projet@gmail.com>
2023-08-30 15:58:07 +0200

    Minor change

--- app/src/main/java/com/example/hellocowcow/ui/screen/RaffleScreen.kt
+++ app/src/main/java/com/example/hellocowcow/ui/screen/RaffleScreen.kt
@@ -2,16 +2,19 @@ package com.example.hellocowcow.ui.screen
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun RaffleScreen() {
     Column(
+        modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Raffle Soon")
+        Text("Raffles Soon")
     }
 }
