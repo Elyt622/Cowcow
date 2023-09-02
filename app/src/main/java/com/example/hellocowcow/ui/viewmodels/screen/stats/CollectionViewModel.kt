e4bc26c189b6cc9885b490272dc76c8b1088bfe2
Yohan <eras.projet@gmail.com>
2023-09-02 03:20:21 +0200

    Replace object by data object

--- app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/stats/CollectionViewModel.kt
+++ app/src/main/java/com/example/hellocowcow/ui/viewmodels/screen/stats/CollectionViewModel.kt
@@ -16,7 +16,7 @@ class CollectionViewModel @Inject constructor(
 ) : ViewModel() {
 
     sealed class UiState {
-        object Loading : UiState()
+        data object Loading : UiState()
         data class Success(val data: CowCollection) : UiState()
         data class Error(val error: String) : UiState()
     }
