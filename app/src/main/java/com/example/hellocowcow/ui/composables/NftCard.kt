ceb668238949b60f2fc3a384c5b412e8cb8d2160
Yohan <eras.projet@gmail.com>
2023-11-01 14:11:42 +0100

    Add NftCard to use in MarketScreen, StakeScreen & WalletScreen

---
+++ app/src/main/java/com/example/hellocowcow/ui/composables/NftCard.kt
@@ -0,0 +1,86 @@
+package com.example.hellocowcow.ui.composables
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Upgrade
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
+import com.bumptech.glide.integration.compose.GlideImage
+import com.example.hellocowcow.domain.models.DomainNft
+import com.example.hellocowcow.ui.theme.Typography2
+
+@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
+@Composable
+fun NftCard(nft: DomainNft, onClicked: () -> Unit) {
+
+    val cardColors = CardDefaults.cardColors(
+        containerColor = MaterialTheme.colorScheme.primary,
+        contentColor = Color.Black
+    )
+
+    ElevatedCard(
+        colors = cardColors,
+        modifier = Modifier.padding(8.dp),
+        elevation = CardDefaults.cardElevation(8.dp),
+        onClick = {
+            onClicked()
+        }
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator(
+                modifier = Modifier.size(16.dp),
+                color = MaterialTheme.colorScheme.background
+            )
+            GlideImage(
+                model = nft.url,
+                contentDescription = nft.collection,
+                modifier = Modifier
+                    .padding(
+                        start = 8.dp,
+                        end = 8.dp,
+                        top = 8.dp
+                    )
+            )
+            if (nft.hasSecondNFT == true)
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .align(Alignment.TopEnd),
+                    imageVector = Icons.Filled.Upgrade,
+                    contentDescription = Icons.Filled.Upgrade.name,
+                    tint = MaterialTheme.colorScheme.background
+                )
+        }
+
+        Column(
+            Modifier
+                .align(Alignment.CenterHorizontally)
+                .padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                color = MaterialTheme.colorScheme.background,
+                text = nft.name.toString(),
+                style = Typography2.bodyLarge
+            )
+        }
+    }
+}
--- app/src/main/java/com/example/hellocowcow/ui/screen/profile/tabs/MarketScreen.kt
+++ app/src/main/java/com/example/hellocowcow/ui/screen/profile/tabs/MarketScreen.kt
@@ -1,23 +1,14 @@
 package com.example.hellocowcow.ui.screen.profile.tabs
 
-import android.content.Intent
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Upgrade
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -28,28 +19,19 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import com.bumptech.glide.integration.compose.ExperimentalGlideComposeApi
-import com.bumptech.glide.integration.compose.GlideImage
-import com.example.hellocowcow.app.module.nft.NftActivity
+import com.example.hellocowcow.ui.composables.NftCard
 import com.example.hellocowcow.ui.theme.Typography2
 import com.example.hellocowcow.ui.viewmodels.screen.profile.MarketViewModel
 import es.dmoral.toasty.Toasty
 
-@OptIn(ExperimentalGlideComposeApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun MarketScreen(
     viewModel: MarketViewModel
 ) {
     val uiState by viewModel.uiState.collectAsState()
     val context = LocalContext.current
-
-    val cardColors = CardDefaults.cardColors(
-        containerColor = MaterialTheme.colorScheme.primary,
-        contentColor = Color.Black
-    )
 
     val functionExecuted = remember { mutableStateOf(false) }
 
@@ -78,56 +60,9 @@ fun MarketScreen(
                     columns = GridCells.Adaptive(150.dp),
                     content = {
                         items(nfts) { nft ->
-                            ElevatedCard(
-                                colors = cardColors,
-                                modifier = Modifier.padding(8.dp),
-                                elevation = CardDefaults.cardElevation(8.dp),
-                                onClick = {
-                                    val intent = Intent(
-                                        context,
-                                        NftActivity::class.java
-                                    )
-                                    intent.putExtra("IDENTIFIER", nft.identifier)
-                                    context.startActivity(intent)
-                                }
-                            ) {
-                                Box {
-                                    CircularProgressIndicator(
-                                        modifier = Modifier.size(16.dp),
-                                        color = MaterialTheme.colorScheme.background
-                                    )
-                                    GlideImage(
-                                        model = nft.url,
-                                        contentDescription = nft.collection,
-                                        modifier = Modifier
-                                            .padding(
-                                                start = 8.dp,
-                                                end = 8.dp,
-                                                top = 8.dp
-                                            )
-                                    )
-                                    if (nft.hasSecondNFT == true)
-                                        Icon(
-                                            modifier = Modifier
-                                                .padding(8.dp)
-                                                .align(Alignment.TopEnd),
-                                            imageVector = Icons.Filled.Upgrade,
-                                            contentDescription = Icons.Filled.Upgrade.name,
-                                            tint = MaterialTheme.colorScheme.background
-                                        )
-                                }
-
-                                Column(
-                                    Modifier
-                                        .align(Alignment.CenterHorizontally)
-                                        .padding(8.dp),
-                                    horizontalAlignment = Alignment.CenterHorizontally
-                                ) {
-                                    Text(
-                                        color = MaterialTheme.colorScheme.background,
-                                        text = nft.name.toString(),
-                                        style = Typography2.bodyLarge
-                                    )
+                            NftCard(nft = nft) {
+                                nft.identifier?.let {
+                                    onClicked(context, it)
                                 }
                             }
                         }
